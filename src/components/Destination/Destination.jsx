488617ac76940e6e042ba7065601719b5e50009f
Kalizar <kalinangelov@abv.bg>
2024-05-04 15:44:21 +0300

    Create Destination page

---
+++ src/components/Destination/Destination.jsx
@@ -0,0 +1,11 @@
+import styles from "./style/Destination.module.css";
+
+const Destination = () => {
+  return (
+    <section className={styles.destinationSection}>
+      <h1>Hello From Destination page</h1>
+    </section>
+  );
+};
+
+export default Destination;
