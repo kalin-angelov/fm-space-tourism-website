a4930964a89c52e4540512b586932178af30b4b4
Kalizar <kalinangelov@abv.bg>
2024-05-04 15:45:28 +0300

    Create Technology page

---
+++ src/components/Technology/Technology.jsx
@@ -0,0 +1,11 @@
+import styles from "./style/Technology.module.css";
+
+const Technology = () => {
+  return (
+    <section className={styles.technologySection}>
+      <h1>Hello from Technology page</h1>
+    </section>
+  );
+};
+
+export default Technology;
