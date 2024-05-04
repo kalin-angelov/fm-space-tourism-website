f3d9753572c9f401738a007d551e431bb3805e42
Kalizar <kalinangelov@abv.bg>
2024-05-04 15:45:11 +0300

    update

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,19 +4,40 @@ import { Link } from "react-router-dom";
 
 import Logo from "/public/shared/Logo";
 import IconHamburger from "/public/shared/IconHamburger";
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
+  const [windowWith, setWindowWith] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowWith = () => {
+      setWindowWith(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowWith);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowWith);
+    };
+
+  },[]);
+
   return (
     <nav className={styles.navBar}>
-      <Link><Logo /></Link>
-      <Link><IconHamburger /></Link>
-      
-      {/* <ul>
-          <li>00 </li>
-          <li>01</li>
-          <li>02</li>
-          <li>03</li>
-      </ul> */}
+      <Link to="/" ><Logo /></Link>
+      {windowWith <= 425 ?
+        <IconHamburger />
+        :
+        <>
+          <hr />
+          <ul className={styles.navLinks}>
+            <li><Link to="/"><span>00</span> Home</Link></li>
+            <li><Link to="/destination"><span>01</span> Destination</Link></li>
+            <li><Link to="/crew"><span>02</span> Crew</Link></li>
+            <li><Link to="/technology"><span>03</span> Technology</Link></li>
+          </ul>
+      </>
+    }
     </nav>
   );
 };
