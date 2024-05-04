96ab067917df429094c9d098cb122c33e0427660
Kalizar <kalinangelov@abv.bg>
2024-05-04 15:43:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import "./App.css";
 
-import Home from "./components/Home/Home";
+import { Route, Routes } from "react-router-dom";
+
 import NavBar from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home";
+import Destination from "./components/Destination/Destination";
+import Crew from "./components/Crew/Crew";
+import Technology from "./components/Technology/Technology";
 
 function App() {
 
   return (
     <main>
       <NavBar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
+      </Routes>
     </main>
   )
 };
