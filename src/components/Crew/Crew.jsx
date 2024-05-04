0d80a563e831442fffde12b32b54f31152d8e765
Kalizar <kalinangelov@abv.bg>
2024-05-04 15:44:06 +0300

    Create Crew page

---
+++ src/components/Crew/Crew.jsx
@@ -0,0 +1,11 @@
+import styles from "./style/Crew.module.css";
+
+const Crew = () => {
+  return (
+    <section className={styles.crewSection}>
+      <h1>Hello from Crew Page</h1>
+    </section>
+  )
+}
+
+export default Crew
