79193c3ff98cc6e85c802b5475f847bde2a13b9f
Kalizar <kalinangelov@abv.bg>
2024-05-07 14:08:10 +0300

    Update BurgerMenu.jsx

--- src/components/NavBar/BurgerMenu.jsx
+++ src/components/NavBar/BurgerMenu.jsx
@@ -6,17 +6,17 @@ import IconClose from "/public/shared/IconClose";
 
 const BurgerMenu = () => {
   return (
-    <aside className={styles.burgerMenu} style={{baseline: true}}>
-        <div className={styles.closeButton}>
-            <IconClose />
-        </div>
-        <ul className={styles.burgerMenuNavLinks}>
-            <li><Link to="/"><span>00</span> Home</Link></li>
-            <li><Link to="/destination"><span>01</span> Destination</Link></li>
-            <li><Link to="/crew"><span>02</span> Crew</Link></li>
-            <li><Link to="/technology"><span>03</span> Technology</Link></li>
-          </ul>
-    </aside>
+    <>
+      <div className={styles.closeButton}>
+          <IconClose />
+      </div>
+      <ul className={styles.burgerMenuNavLinks}>
+          <li><Link to="/"><span>00</span> Home</Link></li>
+          <li><Link to="/destination"><span>01</span> Destination</Link></li>
+          <li><Link to="/crew"><span>02</span> Crew</Link></li>
+          <li><Link to="/technology"><span>03</span> Technology</Link></li>
+        </ul>
+    </>
   );
 };
 
