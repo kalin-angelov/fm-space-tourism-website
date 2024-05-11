ff0d78fd7822b35522e132dc3e84a2b015dff6a6
Kalizar <kalinangelov@abv.bg>
2024-05-11 16:00:29 +0300

    update

---
+++ src/assets/shared/IconHamburger.jsx
@@ -0,0 +1,13 @@
+const IconHamburger = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
+          <path
+            fill="#D0D6F9"
+            fillRule="evenodd"
+            d="M0 0h24v3H0zm0 9h24v3H0zm0 9h24v3H0z"
+          ></path>
+        </svg>
+    );
+};
+
+export default IconHamburger;
