013911c627588b8cb64dd8e2c404abc9ac7428ed
Kalizar <kalinangelov@abv.bg>
2024-05-14 12:59:13 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import { Context } from "./context/Context";
 
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
@@ -9,17 +12,37 @@ import Crew from "./components/Crew/Crew";
 import Technology from "./components/Technology/Technology";
 
 function App() {
+  const [windowWith, setWindowWith] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handlerWindowWith = () => {
+      setWindowWith(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handlerWindowWith);
+
+    return () => {
+      window.removeEventListener("resize", handlerWindowWith)
+    };
+
+  },[]);
+
+  const contextValue = {
+    windowWith,
+  }
 
   return (
-    <main>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/destination" element={<Destination />} />
-        <Route path="/crew" element={<Crew />} />
-        <Route path="/technology" element={<Technology />} />
-      </Routes>
-    </main>
+    <Context.Provider value={contextValue}>
+      <main>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      </main>
+    </Context.Provider>
   )
 };
 
