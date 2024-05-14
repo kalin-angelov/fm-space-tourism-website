1f0cc076a4bb2e5ec2f78ebb8f71b53e8b1dd20b
Kalizar <kalinangelov@abv.bg>
2024-05-14 14:00:27 +0300

    save

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,10 +5,10 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <section className={styles.homeSection}>
+    <div className={styles.homeSection}>
       <article className={styles.container}>
-        <p className={styles.header}>So, you want to travel to</p>
-        <h1>Space</h1>
+        <h1 className={styles.header}>So, you want to travel to</h1>
+        <h2>Space</h2>
         <p className={styles.description}>
           Let’s face it; if you want to go to space, you might as well genuinely go to 
           outer space and not hover kind of on the edge of it. Well sit back, and relax 
@@ -21,8 +21,7 @@ const Home = () => {
           className={styles.exploreBtn}
         >Explore</button>
       </div>
-      
-    </section>
+    </div>
   );
 };
 
