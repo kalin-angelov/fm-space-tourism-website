492c8b8b7880fc8ab66448213b12299ab85ce574
Kalizar <kalinangelov@abv.bg>
2024-05-14 08:39:31 +0300

    Update Technology.jsx

--- src/components/Technology/Technology.jsx
+++ src/components/Technology/Technology.jsx
@@ -1,11 +1,60 @@
 import styles from "./style/Technology.module.css";
 
+import data from "../../db/data.json";
+
+import { useState } from "react";
+
 const Technology = () => {
+  const [tech, setTech] = useState(data.technology[0]);
+  
   return (
     <section className={styles.technologySection}>
-      <h1>Hello from Technology page</h1>
+      <h1><span>03</span> Space launch 101</h1>
+      <div className={styles.machine}>
+        <figure className={styles.imgContainer}>
+          <img src={tech.images.landscape} alt={tech.name} />
+        </figure>
+      
+        <article className={styles.machineInformation}>
+          <ul className={styles.machineList}>
+            <li>
+              <button
+                onClick={() => {setTech(data.technology[0])}}
+                style={tech.name === "Launch vehicle" ? 
+                  {backgroundColor: "white", color: "black"} 
+                  : 
+                  {backgroundColor: "transparent", color: "white"}
+                }
+              >1</button>
+            </li>
+            <li>
+               <button
+                onClick={() => {setTech(data.technology[1])}}
+                style={tech.name === "Spaceport" ? 
+                  {backgroundColor: "white", color: "black"} 
+                  :
+                  {backgroundColor: "transparent", color: "white"}
+                }
+              >2</button>
+            </li>
+            <li>
+              <button
+                onClick={() => {setTech(data.technology[2])}}
+                style={tech.name === "Space capsule" ? 
+                  {backgroundColor: "white", color: "black"} 
+                  : 
+                  {backgroundColor: "transparent", color: "white"}
+                }
+              >3</button>
+            </li>
+          </ul>
+          <h2>The terminology ...</h2>
+          <h3>{tech.name}</h3>
+          <p>{tech.description}</p>
+        </article>
+      </div>
     </section>
-  );
+  )
 };
 
 export default Technology;
